df70c752c23bab23ed0652e9d483ed3c894b4199
Nishant Karajgikar <nishant.karajgikar@mapbox.com>
2021-05-13 10:45:41 -0400

    Move mapCameraOptions to CameraAnimationsManager

--- Sources/MapboxMaps/MapView/Configuration/MapConfig.swift
+++ Sources/MapboxMaps/MapView/Configuration/MapConfig.swift
@@ -2,10 +2,7 @@ import Foundation
 
 /// `MapConfig` is the structure used to configure the map with a set of capabilities
 public struct MapConfig: Equatable {
-
-    /// Used to configure the camera of the map
-    public var camera: MapCameraOptions = MapCameraOptions()
-
+    
     public var render: RenderOptions = RenderOptions()
 
     public var annotations: AnnotationOptions = AnnotationOptions()
